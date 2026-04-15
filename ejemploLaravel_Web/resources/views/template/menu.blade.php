912dedd1f91a51112deeab45339b353c1dee10a9
SebasMartinez21 <jhomartinezma@unal.edu.co>
2026-04-15 11:12:57 -0500

    Templates

---
+++ ejemploLaravel_Web/resources/views/template/menu.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<!--
+	Paradigm Shift by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Paradigm Shift by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Section -->
+					<section id="first">
+						<header>
+							<h2>Magna sed nullam nisl adipiscing</h2>
+						</header>
+						<div class="content">
+							<p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+							<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+						</div>
+					</section>
--- ejemploLaravel_Web/resources/views/welcome.blade.php
+++ ejemploLaravel_Web/resources/views/welcome.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo de laravel</title>
-</head>
-<body>
-    <h1>Hola desde blade.php</h1>
-</body>
-</html>
+@extends('template.menu')
+
+@section('content')
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit architecto vero voluptas nostrum vitae, deserunt libero repudiandae quas laboriosam beatae! Enim, distinctio odit! Rem velit modi, nulla nesciunt cum totam? </div>
+@endsection
+
+@extends('template.footer')
